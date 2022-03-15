d16c28a3bcd86d8732b76cf0171febe14f3f1221
aalcocer <aalcocer@paypal.com>
2022-03-14 23:58:02 -0600

    resize listener and game constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,5 @@
 export const DINO_W = 24;
 export const DINO_H = 24;
 export const BLOCK_SIZE = 32;
+export const GAME_WIDTH = 640;
+export const GAME_HEIGHT = 640;
--- src/game.ts
+++ src/game.ts
@@ -2,9 +2,15 @@ import "phaser";
 import * as Scenes from "./scenes";
 const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
-  backgroundColor: "#125555",
-  width: 840,
-  height: 840,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    parent: "app",
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: 640,
+    height: 740,
+  },
+  backgroundColor: "#000000",
+
   scene: [Scenes.Map_1, Scenes.Map_2],
   physics: {
     default: "arcade",
@@ -16,3 +22,28 @@ const config: Phaser.Types.Core.GameConfig = {
 };
 
 const game = new Phaser.Game(config);
+function resizeApp() {
+  // Width-height-ratio of game resolution
+  // Replace 360 with your game width, and replace 640 with your game height
+  let game_ratio = 640 / 740;
+
+  // Make div full height of browser and keep the ratio of game resolution
+  let div = document.getElementById("app");
+  div.style.width = window.innerHeight * game_ratio + "px";
+  div.style.height = window.innerHeight + "px";
+
+  // Check if device DPI messes up the width-height-ratio
+  let canvas = document.getElementsByTagName("canvas")[0];
+
+  let dpi_w = parseInt(div.style.width) / canvas.width;
+  let dpi_h = parseInt(div.style.height) / canvas.height;
+
+  let height = window.innerHeight * (dpi_w / dpi_h);
+  let width = height * game_ratio;
+
+  // Scale canvas
+  canvas.style.width = width + "px";
+  canvas.style.height = height + "px";
+}
+
+window.addEventListener("resize", resizeApp);
